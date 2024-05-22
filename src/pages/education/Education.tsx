2d3fa77961188d3b8136bef6a403ca838e925a90
jesusre890 <jesusre890@gmail.com>
2024-05-22 15:59:15 -0300

    refact education

--- src/pages/education/Education.tsx
+++ src/pages/education/Education.tsx
@@ -10,16 +10,30 @@ export default function Education() {
 					className="w-12 bg-amber-500 rounded-none text-gray-800 h-4 flex text-xs justify-center"
 				/>
 				<p className="ml-4 text-sm">src/pages</p>
-				<p className=" font-bold text-blue-gray-50 text-sm">/education</p>
+				<p className=" font-bold text-blue-gray-50 text-sm">/Academic Background</p>
 			</div>
 			<div className=" md:ml-7 ml-4 flex flex-col gap-5">
-				<p className="text-2xl mb-2">Education</p>
+				<p className="text-2xl mb-2">Academic Background</p>
 
 				{/*arc*/}
 				<div className="flex flex-col md:flex-row md:gap-7 md:items-center">
 					<p className=" font-bold">Facultad de Arquitectura</p>
 					<p>National University of Rosario</p>
 					<p className="text-sm ">Architect</p>
+				</div>
+
+				{/*java*/}
+				<div className="flex flex-col md:flex-row md:gap-7 md:items-center">
+					<p className=" font-bold">Java Introduction</p>
+					<p>IT Education</p>
+					<p className="text-sm text-light-green-700">[2024]</p>
+				</div>
+
+				{/*sql*/}
+				<div className="flex flex-col md:flex-row md:gap-7 md:items-center">
+					<p className=" font-bold">SQL Database</p>
+					<p>IT Education</p>
+					<p className="text-sm text-light-green-700">[2024]</p>
 				</div>
 
 				{/*inspeccion*/}
